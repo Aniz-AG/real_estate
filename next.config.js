0a8d5a39e9e96f09e0dab24883116cdf2a4ee099
Aniz-AG <azin160404@gmail.com>
2026-01-29 21:41:04 +0530

    Add security headers

--- next.config.js
+++ next.config.js
@@ -15,6 +15,23 @@ const nextConfig = {
         TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
         REDIS_URI: process.env.REDIS_URI,
     },
+    // Security Headers
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: 'X-DNS-Prefetch-Control', value: 'on' },
+                    { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+                    { key: 'X-Content-Type-Options', value: 'nosniff' },
+                    { key: 'X-Frame-Options', value: 'DENY' },
+                    { key: 'X-XSS-Protection', value: '1; mode=block' },
+                    { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+                    { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+                ],
+            },
+        ];
+    },
 }
 
 module.exports = nextConfig
